da18fc9f0dd427acea5fe729b79bd0624a562fce
olegs.osipovs <olegs.osipovs@gmail.com>
2022-06-24 07:43:54 +0300

    Add Actor service and View part added.

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,44 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _dbContext;
+        public ActorsService(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+
+        public void Add(Actor actor)
+        {
+            _dbContext.Actors.Add(actor);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var results = await _dbContext.Actors.ToListAsync();
+            return results;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -11,10 +11,13 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
         [Display(Name ="Profile Picture URL")]
+        [Required(ErrorMessage ="Profile picture is required")]
         public string ProfilePictureUrl { get; set; }
         [Display(Name ="Full Name")]
+        [Required(ErrorMessage = "Full Name is requird")]
         public string FullName { get; set; }
         [Display(Name ="Biography")]
+        [Required(ErrorMessage = "Bio is required")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actors_Movies { get; set; }
