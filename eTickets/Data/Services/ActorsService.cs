af20c1b29ed69999105f2d0a8d9fb4aa64f07286
olegs.osipovs <olegs.osipovs@gmail.com>
2022-06-27 08:37:17 +0300

    Generic entity base repository added.

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -1,4 +1,5 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,44 +8,8 @@ using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public class ActorsService : IActorsService
+    public class ActorsService :EntityBaseRepository<Actor>, IActorsService
     {
-        private readonly AppDbContext _dbContext;
-        public ActorsService(AppDbContext appDbContext)
-        {
-            _dbContext = appDbContext;
-        }
-
-        public async Task AddAsync(Actor actor)
-        {
-            await _dbContext.Actors.AddAsync(actor);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var result = await _dbContext.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            _dbContext.Actors.Remove(result);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var results = await _dbContext.Actors.ToListAsync();
-            return results;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result = await _dbContext.Actors.FirstOrDefaultAsync(a => a.Id == id);
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            _dbContext.Update(newActor);
-            await _dbContext.SaveChangesAsync();
-            return newActor;
-        }
+        public ActorsService(AppDbContext context) : base(context) { }  
     }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eTickets.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
-        public int Id { get; set; }
+        public int Id { get; set; } 
         [Display(Name ="Profile Picture URL")]
         [Required(ErrorMessage ="Profile picture is required")]
         public string ProfilePictureUrl { get; set; }
