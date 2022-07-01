2c87392b3931da48137bcba27d3cffa9a02d467b
olegs.osipovs <olegs.osipovs@gmail.com>
2022-07-01 07:57:09 +0300

    Movie add/edit/delete actions added. Search functionality added

--- eTickets/Data/Services/MoviesService.cs
+++ eTickets/Data/Services/MoviesService.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -16,6 +17,38 @@ namespace eTickets.Data.Services
             _context = context;
         }
 
+        public async Task AddNewMovieAsync(NewMovieVM newMovie)
+        {
+            var movie = new Movie
+            {
+                Name = newMovie.Name,
+                Description = newMovie.Description,
+                Price = newMovie.Price,
+                ImageUrl = newMovie.ImageURL,
+                CinemaId = newMovie.CinemaId,
+                StartDate = newMovie.StartDate,
+                EndDate = newMovie.EndDate,
+                MovieCategory = newMovie.MovieCategory,
+                ProducerId = newMovie.ProducerId
+            };
+
+            await _context.Movies.AddAsync(movie);
+            await _context.SaveChangesAsync();
+
+            foreach (var actorId in newMovie.ActorIds)
+            {
+                var newActorMovie = new Actor_Movie()
+                {
+                    MovieId = movie.Id,
+                    ActorId = actorId
+                };
+
+                await _context.Actors_Movies.AddAsync(newActorMovie);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<Movie> GetMovieByIdAsync(int id)
         {
             var movieDetails = _context.Movies
@@ -25,6 +58,52 @@ namespace eTickets.Data.Services
                 .ThenInclude(a => a.Actor)
                 .FirstOrDefaultAsync(n => n.Id == id);
             return await movieDetails;
+        }
+
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues()
+        {
+            var response = new NewMovieDropdownsVM();
+            response.Actors = await _context.Actors.OrderBy(n => n.FullName).ToListAsync();
+            response.Cinemas = await _context.Cinemas.OrderBy(m => m.Name).ToListAsync();
+            response.Producers = await _context.Producers.OrderBy(p => p.FullName).ToListAsync();
+
+            return response;
+        }
+
+        public async Task UpdateMovieAsync(NewMovieVM data)
+        {
+            var dbMovie = await _context.Movies.FirstOrDefaultAsync(n => n.Id == data.Id);
+
+            if (dbMovie != null)
+            {
+                dbMovie.Name = data.Name;
+                dbMovie.Description = data.Description;
+                dbMovie.Price = data.Price;
+                dbMovie.ImageUrl = data.ImageURL;
+                dbMovie.CinemaId = data.CinemaId;
+                dbMovie.StartDate = data.StartDate;
+                dbMovie.EndDate = data.EndDate;
+                dbMovie.MovieCategory = data.MovieCategory;
+                dbMovie.ProducerId = data.ProducerId;
+                await _context.SaveChangesAsync();
+            }
+
+            //Remove existing actors
+            var existingActorsDb = _context.Actors_Movies.Where(n => n.MovieId == data.Id).ToList();
+            _context.Actors_Movies.RemoveRange(existingActorsDb);
+            await _context.SaveChangesAsync();
+
+            //Add Movie Actors
+            foreach (var actorId in data.ActorIds)
+            {
+                var newActorMovie = new Actor_Movie()
+                {
+                    MovieId = data.Id,
+                    ActorId = actorId
+                };
+                await _context.Actors_Movies.AddAsync(newActorMovie);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ eTickets/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Actors = new List<Actor>();
+            Cinemas = new List<Cinema>();
+            Producers = new List<Producer>();
+        }
+        public List<Actor> Actors { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Producer> Producers { get; set; }
+    }
+}
