792253d7b83d0f78ce233ae9ca07115da392d1fa
olegs.osipovs <olegs.osipovs@gmail.com>
2022-07-13 07:44:55 +0300

    Order, Shopping cart  added.

---
+++ eTickets/Migrations/20220713041407_ShoppingCartItem_IdImproved.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class ShoppingCartItem_IdImproved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItems");
+        }
+    }
+}
